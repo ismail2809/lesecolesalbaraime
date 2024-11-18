bde7e323f0b7def96f156bf31e1941116d5c53a5
ismail2809 <ismaelabounasr@gmail.com>
2024-11-18 16:53:36 +0100

    maj user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
-{
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel; 
+ 
+class User extends Authenticatable implements FilamentUser
+{  
+ 
     use HasFactory, Notifiable;
 
     /**
@@ -43,5 +46,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }
